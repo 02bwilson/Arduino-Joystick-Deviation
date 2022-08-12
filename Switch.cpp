97f2a95c6558a255d10e2daa84b3bd58c036530a
Bryce W <58643544+02bwilson@users.noreply.github.com>
2022-08-11 20:15:40 -0400

    Update Switch.cpp

--- Switch.cpp
+++ Switch.cpp
@@ -1,10 +1,13 @@
+// Define the pins for the Joystick
 #define VRX A5
 #define VRY A4
 #define SW A3
 
+// Base readings of the joystick
 const int XBASE = 511;
 const int YBASE = 507;
 const int SWBASE = 390;
+// % that joystick must move to trigger deviation condition
 const double DEVIATION = .10;
 
 int xRead;
@@ -25,6 +28,7 @@ void loop() {
     Serial.println(XBASE * (1.0 - DEVIATION));
     Serial.println("X PAST DEVIATION: READING: " + static_cast<String>(xRead));
   }
+  // Determine if Y is past deviation
   if(YBASE * (1.0 - DEVIATION >= yRead || YBASE * (1.0 + DEVIATION) <= yRead)){
     Serial.println(YBASE * (1.0 - DEVIATION));
     Serial.println("Y PAST DEVIATION: READING: " + static_cast<String>(yRead));

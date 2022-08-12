a17b66c0bb53858f40e3f8513289deeb9ecd1266
Bryce W <58643544+02bwilson@users.noreply.github.com>
2022-08-11 20:12:12 -0400

    Create Switch.ino

---
+++ Switch.ino
@@ -0,0 +1,33 @@
+#define VRX A5
+#define VRY A4
+#define SW A3
+
+const int XBASE = 511;
+const int YBASE = 507;
+const int SWBASE = 390;
+const double DEVIATION = .10;
+
+int xRead;
+int yRead;
+int swRead;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(500);
+  xRead = analogRead(VRX);
+  yRead = analogRead(VRY);
+  swRead = analogRead(SW);
+  // Determine if X is past deviation
+  if(XBASE * (1.0 - DEVIATION >= xRead || XBASE * (1.0 + DEVIATION) <= xRead)){
+    Serial.println(XBASE * (1.0 - DEVIATION));
+    Serial.println("X PAST DEVIATION: READING: " + static_cast<String>(xRead));
+  }
+  if(YBASE * (1.0 - DEVIATION >= yRead || YBASE * (1.0 + DEVIATION) <= yRead)){
+    Serial.println(YBASE * (1.0 - DEVIATION));
+    Serial.println("Y PAST DEVIATION: READING: " + static_cast<String>(yRead));
+  }
+  Serial.println("TICK END");
+}
